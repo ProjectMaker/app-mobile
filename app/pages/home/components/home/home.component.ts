4c80a459b313b8928c497bf4c840645e33b7eb2f
Thomas MICHELET <thomas@mobilispro.com>
2017-10-18 10:41:11 +0200

    [#22] fix issue

--- app/pages/home/components/home/home.component.ts
+++ app/pages/home/components/home/home.component.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'app-home',
   templateUrl: './home.html'
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+  protected news:Array<any> = [];
+  protected places:Array<any> = [];
+  public ngOnInit() {
+    this.news = [];
+
+    this.places = [{
+      name: 'Le bouillon belge',
+      users: [{
+        pseudo: 'Snoop dog',
+        contents: [{
+          type: 'photo',
+          name: 'Yeah photos',
+          images: ['~/images/friend.png','~/images/friend.png','~/images/friend.png']
+        }, {
+          type: 'comment',
+          name: 'It\'s amazing'
+        }]
+      }],
+
+    },{
+      name: 'Paris Hanoi',
+      users: [{
+        pseudo: 'Busta Rhymes',
+        contents: [{
+          type: 'comment',
+          name: 'It\'s amazing'
+        },{
+          type: 'photo',
+          name: 'Yeah photos',
+          images: ['~/images/friend.png']
+        }]
+      }],
+
+    }]
+  }
+
+  protected templateSelector(item: any, index: number, items: any) {
+    return item.type;
+  }
+}
