8b861e52336c17f8e87674fdab0b4ae01201e49e
Thomas MICHELET <thomas@mobilispro.com>
2017-10-24 12:29:23 +0200

    [#41] create FriendService

--- app/core/services/index.ts
+++ app/core/services/index.ts
@@ -1,10 +1,13 @@
 import { PlaceService } from './place/place.service';
 import { NewsFeedService } from './news-feed/news-feed.service';
+import { FriendService } from './friend/friend.service';
 
 export * from './place/place.service';
 export * from './news-feed/news-feed.service';
+export * from './friend/friend.service';
 
 export const APP_SERVICES = [
   PlaceService,
-  NewsFeedService
+  NewsFeedService,
+  FriendService
 ];
