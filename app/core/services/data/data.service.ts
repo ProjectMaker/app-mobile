74cf74e4e4c091c7f3559d734d6b12b216e5a20f
Thomas MICHELET <thomas@mobilispro.com>
2017-10-24 12:14:39 +0200

    [#41] clean DataService

--- app/core/services/data/data.service.ts
+++ app/core/services/data/data.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 
-const API_URL = 'http://private-5d708-interviewfront.apiary-mock.com/transactions';
-
 @Injectable()
 export class DataService {
   public getPlace():any {
@@ -12,39 +10,5 @@ export class DataService {
     };
 
     return place;
-  }
-
-  public getNews():Array<any> {
-    const places = [{
-      name: 'Le bouillon belge',
-      users: [{
-        pseudo: 'Snoop dog',
-        contents: [{
-          type: 'photo',
-          name: 'Yeah photos',
-          images: ['~/images/friend.png','~/images/friend.png','~/images/friend.png']
-        }, {
-          type: 'comment',
-          name: 'It\'s amazing'
-        }]
-      }],
-
-    },{
-      name: 'Paris Hanoi',
-      users: [{
-        pseudo: 'Busta Rhymes',
-        contents: [{
-          type: 'comment',
-          name: 'It\'s amazing'
-        },{
-          type: 'photo',
-          name: 'Yeah photos',
-          images: ['~/images/friend.png']
-        }]
-      }],
-
-    }];
-
-    return places;
   }
 }
