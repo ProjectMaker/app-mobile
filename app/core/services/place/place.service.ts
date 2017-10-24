c2bba1b590c3a6750a6d81831fccf8ff43c478e6
Thomas MICHELET <thomas@mobilispro.com>
2017-10-24 12:54:40 +0200

    [#41] place structure address

--- app/core/services/place/place.service.ts
+++ app/core/services/place/place.service.ts
@@ -6,7 +6,13 @@ export class PlaceService {
   public getPlace():any {
     const place = {
       _id: 1, name: 'Le bouillon belge',
-      address: '46 rue des Haies', phone: '+33665433455', openHours: '11:30 - 19:00', contexts: ['friend','couple']
+      address: {
+        streetNumber: '46',
+        route: 'rue des haies',
+        city: 'Paris',
+        postalCode: '75020'
+      },
+      phone: '+33665433455', openHours: '11:30 - 19:00', contexts: ['friend','couple']
     };
 
     return place;
@@ -15,13 +21,31 @@ export class PlaceService {
   public getPlaces():Array<any> {
     const places = [{
       _id: 1, name: 'Le bouillon belge',
-      address: '46 rue des Haies', phone: '+33665433455', openHours: '11:30 - 19:00', contexts: ['friend','couple']
+      address: {
+        streetNumber: '46',
+        route: 'rue des haies',
+        city: 'Paris',
+        postalCode: '75020'
+      },
+      phone: '+33665433455', openHours: '11:30 - 19:00', contexts: ['friend','couple']
     },{
       _id: 2, name: 'Paris Hanoi',
-      address: '46 rue des Haies', phone: '+33665433455', openHours: '11:30 - 19:00', contexts: ['friend','couple']
+      address: {
+        streetNumber: '46',
+        route: 'rue des haies',
+        city: 'Paris',
+        postalCode: '75020'
+      },
+      phone: '+33665433455', openHours: '11:30 - 19:00', contexts: ['friend','couple']
     },{
       _id: 3, name: 'Train bleu',
-      address: '46 rue des Haies', phone: '+33665433455', openHours: '11:30 - 19:00', contexts: ['friend','couple']
+      address: {
+        streetNumber: '46',
+        route: 'rue des haies',
+        city: 'Paris',
+        postalCode: '75020'
+      },
+      phone: '+33665433455', openHours: '11:30 - 19:00', contexts: ['friend','couple']
     }];
 
     return places;
