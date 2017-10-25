41da7c719a7ddaed8e4e021067d035aeda15a611
Thomas MICHELET <thomas@mobilispro.com>
2017-10-25 08:59:32 +0200

    [#45] method search, mocks

--- app/core/services/place/place.service.ts
+++ app/core/services/place/place.service.ts
@@ -3,6 +3,15 @@ import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class PlaceService {
+  private places:Array<any>;
+  private favorites:Array<any> = [];
+
+  public searchPlaces(position:any):Array<any> {
+    const places = require('./search-places.mock.json');
+
+    return places;
+  }
+
   public getPlace():any {
     const place = {
       _id: 1, name: 'Le bouillon belge',
@@ -19,35 +28,8 @@ export class PlaceService {
   }
 
   public getPlaces():Array<any> {
-    const places = [{
-      _id: 1, name: 'Le bouillon belge',
-      address: {
-        streetNumber: '46',
-        route: 'rue des haies',
-        city: 'Paris',
-        postalCode: '75020'
-      },
-      phone: '+33665433455', openHours: '11:30 - 19:00', contexts: ['friend','couple']
-    },{
-      _id: 2, name: 'Paris Hanoi',
-      address: {
-        streetNumber: '46',
-        route: 'rue des haies',
-        city: 'Paris',
-        postalCode: '75020'
-      },
-      phone: '+33665433455', openHours: '11:30 - 19:00', contexts: ['friend','couple']
-    },{
-      _id: 3, name: 'Train bleu',
-      address: {
-        streetNumber: '46',
-        route: 'rue des haies',
-        city: 'Paris',
-        postalCode: '75020'
-      },
-      phone: '+33665433455', openHours: '11:30 - 19:00', contexts: ['friend','couple']
-    }];
+    if (!this.places) this.places = require('./places.mock.json');
 
-    return places;
+    return this.places;
   }
 }
--- app/pages/new-place/components/new-place-select/new-place-select.component.ts
+++ app/pages/new-place/components/new-place-select/new-place-select.component.ts
@@ -19,7 +19,8 @@ export class NewPlaceSelectComponent implements OnInit {
   public constructor(private placeService:PlaceService, private routerExtensions:RouterExtensions) { }
 
   public ngOnInit() {
-    this.places = this.placeService.getPlaces();
+    const position:any = {};
+    this.places = this.placeService.searchPlaces(position);
     this.places.push({
         name: 'Le lieu n\' est pas dans la liste',
         footer: true
